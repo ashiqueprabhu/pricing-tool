1826cfe68ab6586af6e61c666e0e57b0a1af711f
ashiqueprabhu-ak <ashique@frugalscientific.com>
2024-08-12 21:18:38 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Price Estimation Tool
+
+Price Estimation Tool to automate the process of calculating the final unit and total price for quotations
+
+#### License
+
+MIT
---
+++ pricing_tool/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Price Estimation Tool",
+			"type": "module",
+			"label": _("Price Estimation Tool")
+		}
+	]
